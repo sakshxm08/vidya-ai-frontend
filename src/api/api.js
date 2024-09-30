1796463832422e5e8c7fc2fef8fd5d2bdf008a0c
Saksham Gambhir <saksham2211gambhir@gmail.com>
2024-09-30 22:26:43 +0530

    authentication updated

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 // Create an instance of axios with a base URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_ROUTE, // Use the base URL defined in environment variables
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Use the base URL defined in environment variables
+  withCredentials: true,
 });
 
 // Export the axios instance for use in other parts of the application
